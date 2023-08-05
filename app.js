ff75710dad1977b9f709ee02c58e819a0f22af15
YanuarEka <yanuarekasusila@gmail.com>
2023-08-06 00:36:54 +0700

    Adding alert message & fixs title header

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 const methodOverride = require('method-override');
+
+// import express-session & connect flash for adding alert message
+const session = require('express-session');
+const flash = require('connect-flash');
 
 // import mongoose / create connection mongoosejs
 const mongoose = require('mongoose');
@@ -20,6 +25,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
+
+// use express session
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+  })
+);
+
+// use connect-flash
+app.use(flash());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
