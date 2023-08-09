7f5b796b1efb37948d63126c99c971d1c42063eb
YanuarEka <yanuarekasusila@gmail.com>
2023-08-09 22:22:34 +0700

    Adding controller read data feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -314,10 +314,12 @@ module.exports = {
       const alertMessage = req.flash('alertMessage');
       const alertStatus = req.flash('alertStatus');
       const alert = { message: alertMessage, status: alertStatus };
+      const feature = await Feature.find({ itemId: itemId });
       res.render('admin/item/detail_item/view_detail_item', {
         title: 'Staycation | Detail Item',
         alert,
         itemId,
+        feature,
       });
     } catch (error) {
       req.flash('alerMessage', `${error.message}`);
