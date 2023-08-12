034f7d12c68ce8bf9d7bb9e6b294d70950e98afc
YanuarEka <yanuarekasusila@gmail.com>
2023-08-12 21:55:02 +0700

    Adding api for landing page mostPicked & hero

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,6 +1,22 @@
+const Item = require('../models/Item');
+const Traveler = require('../models/Booking');
+const Treasure = require('../models/Activity');
+
 module.exports = {
-  landingPage: (req, res) => {
-    const message = 'Hello World';
-    res.status(200).json({ message });
+  landingPage: async (req, res) => {
+    try {
+      const mostPicked = await Item.find().select('_id title country city price unit imageId').limit(5).populate({ path: 'imageId', select: '_id imageUrl' });
+      const traveler = await Traveler.find();
+      const treasure = await Treasure.find();
+      const city = await Item.find();
+      res.status(200).json({
+        hero: {
+          travelers: traveler.length,
+          treasures: treasure.length,
+          cities: city.length,
+        },
+        mostPicked,
+      });
+    } catch (error) {}
   },
 };
